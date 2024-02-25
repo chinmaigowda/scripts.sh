aa014efc23da8bab5999b509569fb87545b74932
Chinmai H M <chinmaygowda@192.168.1.2>
2024-02-25 13:45:43 +0530

    maxdepth

--- home/ubuntu/scripts/practice_gpt.sh
+++ home/ubuntu/scripts/practice_gpt.sh
@@ -7,7 +7,7 @@ read -p "Enter the directory path: " dir
 #to find the directory path exists or not
 
 if [[ -d $dir ]]; then
-    file_count=$(find $dir -type f | wc -l)
+    file_count=$(find $dir -maxdepth 1 -type f | wc -l)
     echo "Number of files in $dir is $file_count"
 else
     echo "Pass the directory path properly: $dir"
